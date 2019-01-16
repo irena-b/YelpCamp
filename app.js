59e0fc6da6217d39c0d0b9bfb3fbd281ede6da06
Irina Baeva <irinabayova@gmail.com>
2019-01-16 22:41:47 +0300

    new routes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yelp_camp');
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser:true });
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
@@ -11,17 +12,20 @@ app.set('view engine', 'ejs');
 
 const campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String, 
+    description: String
 })
 //compile in model
 const Campground = mongoose.model("Campground", campgroundSchema);
-Campground.create(
-    {name: "Salmon Creec", image: 'https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1541405226/campground-photos/wdzo9tcuyf0pljoh67nn.jpg'
-}, function(err, campground){
-    if(err) {console.log(err)}
-    else{console.log(campground)}
-}
-)
+// Campground.create(
+//     {name: "The best Camp", 
+//     image: 'https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg',
+//     description: "No water, no bath! But cool cux no peeple"
+// }, function(err, campground){
+//     if(err) {console.log(err)}
+//     else{console.log(campground)}
+// }
+// )
 //we move array of our camps out due to hoisting
 let campgrounds = [
     {name: "Salmon Creec", image: 'https://img.hipcamp.com/image/upload/c_limit,f_auto,h_1200,q_60,w_1920/v1541405226/campground-photos/wdzo9tcuyf0pljoh67nn.jpg'},
@@ -34,30 +38,55 @@ app.get('/', function(req, res){
     res.render('landing')
 });
 
+//INDEX route to show our campgrounds
 app.get('/campgrounds', function(req, res){
     //get all campgrounds from DataBase
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err)
         } else{
+            //то есть здесь раньше мы рендерили campgrounds из массива es.render('campgrounds', {campgrounds: сampgrounds});
             res.render('campgrounds', {campgrounds: allCampgrounds});
         }
     })
 });
-
+//NEW show form to create new cmpground
 app.get('/campgrounds/new', function(req, res){
     res.render('new.ejs')
-})
-//post for adding new camps
+});
+//POST for adding new camps
 app.post('/campgrounds', function(req, res){
 //get data from form and add to campgrounds array
    let image = req.body.image;
    let name = req.body.name;
-   let newCampground = {name: name, image: image}; 
-   campgrounds.push(newCampground);
+   let description = req.body.description;
+
+   let newCampground = {name: name, image: image, description: description}; 
+//we don't need this line anymore cuz now we use mongodb   campgrounds.push(newCampground);
+//create new campground and save it to db также как мы делали до этого выше
+Campground.create(newCampground, function (err, newlyCreated){
+    if(err){
+        console.log(err);//here we need to make err page
+    } else{
 //redirect back to campgrounds page
-res.redirect('/campgrounds')
+        res.redirect('/campgrounds')
+    }
 })
+})
+
+//page with more info for every different camp with id
+app.get('/campgrounds/:id', function(req, res){
+    //find the campground with provided ID with new method findbyID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else{
+//render show template with that campground
+res.render('show', {campground: foundCampground})
+        }
+    });
+    
+});
 
 app.listen(3001, ()=>{
     console.log('Yelp Camp is running on 3001')
